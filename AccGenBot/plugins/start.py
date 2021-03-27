f9237caed542cb26fcaa0d641796a3e5ed1812b4
ExploitzBots <79147710+ExploitzBots@users.noreply.github.com>
2021-03-27 20:43:28 +0530

    start.py

--- AccGenBot/plugins/start.py
+++ AccGenBot/plugins/start.py
@@ -10,12 +10,12 @@ async def start(event):
     await AccGenBot.send_message(Config.PRV_CHAT, f"Bot Started By [{event.sender_id}](tg://user?id={event.sender_id})")
     Soul = await verify(Config.CHANNEL_US, event, AccGenBot)
     if Soul is False:
-            await event.reply("**Join my channel to use this bot :)**", buttons=[[Button.url("Join Channel", Config.CHANNEL_URL)]])
+            await event.reply("**Sad , Dude 🥺 Join Channel To Use :)**", buttons=[[Button.url("Join Channel", Config.CHANNEL_URL)]])
             return
 
     heck = await AccGenBot(GetFullUserRequest(event.sender_id))
     name = sender.user.first_name
-    await event.reply("Heya {name}\n\nWelcome to AccGenBot, From Here You can Generate free Accounts\n\nMade With ❤️ By @SoulHackz", buttons=[[Button.inline("Generate Accounts", data="gen")], [Button.url("Join Channel!", Config.CHANNEL_URL)]])
+    await event.reply("Heya {name}\n\nWelcome to Boi 🔥, From Here You can Generate Premium Accounts\n\nMade With ❤️ By @ExploitzBots", buttons=[[Button.inline("Generate Accounts", data="gen")], [Button.url("Join Channel!", Config.CHANNEL_URL)]])
 
 #Repeat Codes :/
 @AccGenBot.on(events.callbackquery.CallbackQuery(data="start_bot"))
@@ -23,9 +23,9 @@ async def start(event):
 
     Soul = await verify(Config.CHANNEL_US, event, AccGenBot)
     if Soul is False:
-            await event.edit("**Join my channel to use this bot :)**", buttons=[[Button.url("Join Channel", Config.CHANNEL_URL)]])
+            await event.edit("**Join My channel to use this bot :)**", buttons=[[Button.url("Join Channel", Config.CHANNEL_URL)]])
             return
 
     heck = await AccGenBot(GetFullUserRequest(event.sender_id))
     name = sender.user.first_name
-    await event.edit("Heya {name}\n\nWelcome to AccGenBot, From Here You can Generate free Accounts\n\nMade With ❤️ By @SoulHackz", buttons=[[Button.inline("Generate Accounts", data="gen")], [Button.url("Join Channel!", Config.CHANNEL_URL)]])
+    await event.edit("Heya {name}\n\nWelcome to AccGenBot, From Here You can Generate free Accounts\n\nMade With ❤️ By @ExploitzBots", buttons=[[Button.inline("Generate Accounts", data="gen")], [Button.url("Join Channel!", Config.CHANNEL_URL)]])
