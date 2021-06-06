2e6727b4011725758af368e5e1d51406198691bf
ExploitzBots <79147710+ExploitzBots@users.noreply.github.com>
2021-06-06 12:21:00 +0530

    Update Vpns.py

--- AccGenBot/plugins/Vpns.py
+++ AccGenBot/plugins/Vpns.py
@@ -8,12 +8,7 @@ from Configs import Config
 @AccGenBot.on(events.callbackquery.CallbackQuery(data="vpns"))
 async def vpns(vpns):
 
-TEXT = """
-**Heya {}**
-Choose the account you wanna generate.
-""".format(vpns.sender.first_name)
-
-     await vpns.edit(TEXT, button=[
+     await vpns.edit("Hehehe", button=[
     [Button.inline("Nord Vpn", data="nord"), Button.inline("Express Vpn", data="exv")],
     [Button.inline("IpVanish", data="ips"), Button.inline("SurfShark", data="sfs")],
     [Button.inline("Surfeasy VPN", data="sfv"), Button.inline("Windscribe", data="wnd")],
