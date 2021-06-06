9e05d32b476cf2fcb2251f6eeadbf88b6ffaacb7
ExploitzBots <79147710+ExploitzBots@users.noreply.github.com>
2021-06-06 11:57:29 +0530

    Update gen.py

--- AccGenBot/plugins/gen.py
+++ AccGenBot/plugins/gen.py
@@ -15,24 +15,8 @@ async def gen(event):
 
     await event.edit("Choose Which Accounts you want to Generate", 
 buttons=[
-    [Button.inline("Vpn", data="Vpns"), Button.inline("Voot", data="voot")],
+    [Button.inline("Vpn Accounts", data="Vpns"), Button.inline("Voot", data="voot")],
     [Button.inline("Wish", data="wish"), Button.inline("Shudder", data="shud")],
     [Button.inline("MeetMe", data="MeetMe"), Button.inline("Uplay", data="Uplay")],
     [Button.inline("Main Menu", data="start_bot")]
 ])
-
-@AccGenBot.on(events.callbackquery.CallbackQuery(data="Vpns"))
-async def gen(event):
-
-    soul = await verify(Config.CHANNEL_US, event, AccGenBot)
-    if soul is False:
-           await event.edit("**Join my channel for using this bot :)**", buttons=[[Button.url("Join Channel!", Config.CHANNEL_URL)]])
-           return
-
-    await event.edit("Select Your Option")
-    buttons=[
-        [Button.inline("IpVanish", data="IPS")],
-        [Button.inline("NordVpn", data="Nord")]
-])
-   
-
