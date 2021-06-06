ba32e409f654acdb34b1b067ff2ace1db1cfae1b
ExploitzBots <79147710+ExploitzBots@users.noreply.github.com>
2021-06-06 19:22:19 +0530

    Create porn.py

---
+++ AccGenBot/plugins/porn.py
@@ -0,0 +1,17 @@
+import random
+from . import *
+from .. import *
+from telethon import events
+from telethon import Button
+from telethon.tl.functions.users import GetFullUserRequest
+from Configs import Config
+
+@AccGenBot.on(events.callbackquery.CallbackQuery(data=b"stream"))
+async def happy(event):
+    text="*Choose The Which Account You Want*"
+    await event.edit(text,
+                     buttons=[
+                         [Button.inline("✘ Pornhub ✘", data="pornhub")],
+                         [Button.inline("✘ Brazzerz ✘", data="brazzerz")],
+                         [Button.inline("Main Menu", data="gen")]
+                     ])
