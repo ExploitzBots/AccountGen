8dd202ae3feffec785c76fcb07702a8621da3b60
ExploitzBots <79147710+ExploitzBots@users.noreply.github.com>
2021-06-06 15:25:32 +0530

    Update Vpns.py

--- AccGenBot/plugins/Vpns.py
+++ AccGenBot/plugins/Vpns.py
@@ -8,9 +8,11 @@ from Configs import Config
 
 @AccGenBot.on(events.callbackquery.CallbackQuery(data=b"vpns"))
 async def sed(event):
-        button=[
-    [Button.inline("Nord Vpn", data="nord"), Button.inline("Express Vpn", data="exv")],
-    [Button.inline("IpVanish", data="ips"), Button.inline("SurfShark", data="sfs")],
-    [Button.inline("Surfeasy VPN", data="sfv"), Button.inline("Windscribe", data="wnd")],
-    [Button.inline("Main Menu", data="start_bot")],
-]
+    text="I am an Account Generator Bot!\nI can generate working accounts for you.\n\nClick generate accounts to get your account!! Make sure to join my channel and support me!"
+    await event.edit(text,
+                     buttons=[
+                         [Button.inline("Nord Vpn", data="nord"), Button.inline("Express Vpn", data="exv")],
+                         [Button.inline("IpVanish", data="ips"), Button.inline("SurfShark", data="sfs")],
+                         [Button.inline("Surfeasy VPN", data="sfv"), Button.inline("Windscribe", data="wnd")],
+                         [Button.inline("Main Menu", data="start_bot"
+                     ])
