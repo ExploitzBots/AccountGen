f465bf9f14cf7cfbfdca3c3a7f2f8cfafb9efcf4
ExploitzBots <79147710+ExploitzBots@users.noreply.github.com>
2021-06-06 08:22:50 +0530

    Update start.py

--- AccGenBot/plugins/start.py
+++ AccGenBot/plugins/start.py
@@ -10,12 +10,12 @@ async def start(event):
     await AccGenBot.send_message(Config.PRV_CHAT, f"Bot Started By [{event.sender_id}](tg://user?id={event.sender_id})")
     Soul = await verify(Config.CHANNEL_US, event, AccGenBot)
     if Soul is False:
-            await event.reply("**Join my channel to use this bot :)**", buttons=[[Button.url("Join Channel", Config.CHANNEL_URL)]])
+            await event.reply("** You Must Join My Channel To Use Bot\nElse Gay! :)**", buttons=[[Button.url("Join Channel", Config.CHANNEL_URL)]])
             return
 
     heck = await AccGenBot(GetFullUserRequest(event.sender_id))
     name = heck.user.first_name
-    await event.reply(f"Heya {name}\n\nWelcome to AccGenBot, From Here You can Generate free Accounts\n\nMade With ❤️ By @ProAccountsHub", buttons=[[Button.inline("Generate Accounts", data="gen")], [Button.url("Join Channel!", Config.CHANNEL_URL)]])
+    await event.reply(f"Heya {name}\n\nWelcome to **MultiGenBot**, From Here You Can Generate Many Premium Accounts\n\nMade With ❤️ By @EvilzBots, buttons=[[Button.inline("Generate Accounts", data="gen")], [Button.url("Join Channel!", Config.CHANNEL_URL)]])
 
 #Repeat Codes :/
 @AccGenBot.on(events.callbackquery.CallbackQuery(data="start_bot"))
@@ -28,4 +28,4 @@ async def start(event):
 
     heck = await AccGenBot(GetFullUserRequest(event.sender_id))
     name = heck.user.first_name
-    await event.edit(f"Hoi ❤️ \n\nWelcome to AccGenBot, From Here You can Generate Accounts That You Want \n\nMade With ❤️ By @ProAccountsHub", buttons=[[Button.inline("Generate Accounts", data="gen")], [Button.url("Join Channel!", Config.CHANNEL_URL)]])
+    await event.edit(f"Heya {name}\n\nWelcome to **MultiGenBot**, From Here You Can Generate Many Premium Accounts\n\nMade With ❤️ By @EvilzBots, buttons=[[Button.inline("Generate Accounts", data="gen")], [Button.url("Join Channel!", Config.CHANNEL_URL)]])
