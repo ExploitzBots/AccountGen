4102bc55aafae1a4d176ac66c3309e594148559d
ExploitzBots <79147710+ExploitzBots@users.noreply.github.com>
2021-06-06 08:22:26 +0530

    Update gen.py

--- AccGenBot/plugins/gen.py
+++ AccGenBot/plugins/gen.py
@@ -15,8 +15,10 @@ async def gen(event):
 
     await event.edit("Choose Which Accounts you want to Generate", 
 buttons=[
-    [Button.inline("Zee5", data="zee5"), Button.inline("Voot", data="voot")],
-    [Button.inline("Wish", data="wish"), Button.inline("Shudder", data="shud")],
-    [Button.inline("MeetMe", data="MeetMe"), Button.inline("Uplay", data="Uplay")],
+    [Button.inline("Vpn Section", data="vpns"), Button.inline("Streaming Section", data="str_sec")],
     [Button.inline("Main Menu", data="start_bot")]
+vpns=[
+  [Button.inline("Nord Vpn", data="nord"), Button.inline("IPVansih", data="ips")],
+    [Button.inline("Main Menu", data="start_bot")]
+    
 ])
