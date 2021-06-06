f0e05796be00b72ba3d588d0ed0efe2564a60810
ExploitzBots <79147710+ExploitzBots@users.noreply.github.com>
2021-06-06 12:23:07 +0530

    Update Vpns.py

--- AccGenBot/plugins/Vpns.py
+++ AccGenBot/plugins/Vpns.py
@@ -6,9 +6,8 @@ from telethon.tl.functions.users import GetFullUserRequest
 from Configs import Config
 
 @AccGenBot.on(events.callbackquery.CallbackQuery(data="vpns"))
-async def vpns(vpns):
 
-     await vpns.edit("Hehehe", button=[
+button=[
     [Button.inline("Nord Vpn", data="nord"), Button.inline("Express Vpn", data="exv")],
     [Button.inline("IpVanish", data="ips"), Button.inline("SurfShark", data="sfs")],
     [Button.inline("Surfeasy VPN", data="sfv"), Button.inline("Windscribe", data="wnd")],
