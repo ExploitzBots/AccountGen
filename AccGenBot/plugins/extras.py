bc377dcbef6f2b88de37ebe9bc4c675e5af025e2
ExploitzBots <79147710+ExploitzBots@users.noreply.github.com>
2021-06-22 22:35:49 +0530

    Update extras.py

--- AccGenBot/plugins/extras.py
+++ AccGenBot/plugins/extras.py
@@ -1,1 +1,13 @@
-Working On It ...
+import random
+from . import *
+from .. import *
+from telethon import Button, events
+from telethon.tl.functions.users import GetFullUserRequest
+from Configs import Config
+
+@AccGenBot.on(events.NewMessage(pattern="/info"))
+async def info(event):
+    heck = await AccGenBot(GetFullUserRequest(event.sender_id))
+    name = heck.user.first_name
+    await event.reply(f"**📡Your Account Information\n\nUser-ID : {event.sender_id}**")
+
