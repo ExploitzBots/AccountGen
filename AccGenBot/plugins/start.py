8582e952f6f7f58eb457a8afab2fee1be86db946
ExploitzBots <79147710+ExploitzBots@users.noreply.github.com>
2021-06-22 22:42:29 +0530

    Update start.py

--- AccGenBot/plugins/start.py
+++ AccGenBot/plugins/start.py
@@ -3,6 +3,8 @@ from .. import *
 from telethon import Button, events
 from telethon.tl.functions.users import GetFullUserRequest
 from Configs import Config
+from pymongo import MongoClient
+from pymongo.errors import ConnectionFailure
 
 try:
     mongo_client = MongoClient(Config.MONGO_DB)
