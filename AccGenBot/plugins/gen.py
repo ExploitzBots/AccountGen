45c50b77c3fd8959b3c62e9f5a973ec73395160d
ExploitzBots <79147710+ExploitzBots@users.noreply.github.com>
2021-06-22 22:06:56 +0530

    Update gen.py

--- AccGenBot/plugins/gen.py
+++ AccGenBot/plugins/gen.py
@@ -5,19 +5,22 @@ from telethon import Button, events
 from telethon.tl.functions.users import GetFullUserRequest
 from Configs import Config
 
-@AccGenBot.on(events.callbackquery.CallbackQuery(data=b"gen"))
-async def gen(event):
+@AccGenBot.on(events.NewMessage(pattern="/help"))
+async def help(event):
 
+    await AccGenBot.send_message(Config.PRV_CHAT, f"Bot Started By [{event.sender_id}](tg://user?id={event.sender_id})")
     evil = await verify(Config.CHANNEL_US, event, AccGenBot)
     if evil is False:
-           await event.edit("**Join my channel for using this bot :)**", buttons=[[Button.url("Join Channel!", Config.CHANNEL_URL)]])
-           return
+            await event.reply("**Join my channel to use this bot :)**", buttons=[[Button.url("Join Channel", Config.CHANNEL_URL)]])
+            return
 
-    await event.edit("Choose Which Accounts you want to Generate", 
-buttons=[
-    [Button.inline("✘ Vpn Accounts ✘", data="vpns")],
-    [Button.inline("✘ Streaming Accounts ✘", data="stream")],
-    [Button.inline("✘ Cashout Accounts ✘", data="cash")],
-    [Button.inline("✘ Educational Accounts ✘", data="edu")],
-    [Button.inline("Back :)", data="start_bot")]
-])
+    heck = await AccGenBot(GetFullUserRequest(event.sender_id))
+    name = heck.user.first_name
+    await event.reply(f"""
+**I am an Account Generator Bot
+-------------------------------------------------
+I can provide premium accounts of different services
+--------------------------------------------------
+Do /gen to generate an account
+--------------------------------------------------**
+""", buttons=[[Button.url("Join Channel!", Config.CHANNEL_URL)]])
