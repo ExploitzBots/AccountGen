6571062451ef24d312a7c1bc3528a3bca2b20447
ExploitzBots <79147710+ExploitzBots@users.noreply.github.com>
2021-06-23 07:09:06 +0530

    Update acc.py

--- AccGenBot/plugins/acc.py
+++ AccGenBot/plugins/acc.py
@@ -87,7 +87,7 @@ async def aha(event):
 
     hehe = await event.reply("<b><i>Working On It</i></b>", parse_mode="HTML")
 
-        await hehe.edit(f"**Aha Account Is Limited For This Moment, It's Is Exceed**,
+        await hehe.edit(f"**Aha Account Is Limited For This Moment, It's Is Exceed**",
     buttons=[
         [Button.url("Join Channel", Config.CHANNEL_URL)]
     ])
